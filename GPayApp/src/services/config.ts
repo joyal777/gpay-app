cc27fa295149ea83ab4b9c17af67228e27c20996
joyal777 <joyaltony78@gmail.com>
2026-06-19 22:57:21 +0530

    fixed some security issues

--- GPayApp/src/services/config.ts
+++ GPayApp/src/services/config.ts
@@ -1,4 +1,3 @@
-// Your computer's IP address
-const API_URL = process.env.API_URL || 'http://localhost:8000';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000';
 export const API_BASE_URL = `${API_URL}/api`;
 export const IMAGE_BASE = API_URL;
